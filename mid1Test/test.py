08fcb492f462c7567cb5b031730fa04630bc33ee
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-03-27 08:15:58 +0800

    repet

--- mid1Test/test.py
+++ mid1Test/test.py
@@ -1,31 +1,17 @@
+grid=[1]
 while True:
-    try:
-        num=list(map(int,input().split()))
-        round=list(map(int,input().split()))
-        temp=[]
-        temp2=[]
-        ans=[]
-        for s in range(len(num)):   #temp2==num
-            temp2.append(num[s]) 
-
-        for i in range(1,len(round)):
-            j=round[i]
-            while len(temp2)>=j:
-                for l in range(j):
-                    temp.append(temp2.pop(0))
-                temp.reverse()
-                for k in range(len(temp)):
-                    ans.append(temp[k])
-                temp.clear()
-            for z in range(len(temp2)):
-                ans.append(temp2[0])
-                temp2.pop(0)
-            for s in range(len(ans)):
-                temp2.append(ans[s])
-            for x in range(len(num)):
-                ans.pop(0)
-        for a in range(len(temp2)-1):
-            print(temp2[a],end=" ")
-        print(temp2[-1])
-    except:
+    n=int(input())
+    if n==-1:
         break
+    if n+1<=len(grid):
+        print(grid[n])
+    else:
+        for i in range(len(grid)-1,n):
+            temp=grid[i]
+            now=temp[0]
+            count=0
+            ans=""
+            for j in grid:
+                if j!=now:
+                    ans=ans+str(count)+now
+                    
