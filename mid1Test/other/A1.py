929134424dc957aac4e6fcf0c64b5eccf1848f15
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-03-28 06:44:31 +0800

    chagne but have some problem

--- mid1Test/other/A1.py
+++ mid1Test/other/A1.py
@@ -1,17 +1,15 @@
-import copy
-def change(s,x):
-    if x+1==len(s):
-        print(s)
-        return 0
-    else:
-        change(s,x+1)
-        for i in range(x+1,len(s)):
-            temp=copy.deepcopy(s)
-            t=temp[x]
-            temp[x]=temp[i]
-            temp[i]=t
-            change(temp,x+1)
-            
-s=list(input())
-change(s,0)
+# def change(s,x):
+#     if x+1==len(s):
+#         print(s)
+#         return 0
+#     else:
+#         change(s,x+1)
+#         for i in range(x+1,len(s)):
+#             temp=s[:]
+#             temp[x],temp[i]=temp[i],temp[x]
+#             change(temp,x+1)
+# s=list(input())
+# change(s,0)
 
+n=input()
+print(len(s))
