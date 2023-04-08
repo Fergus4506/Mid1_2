44baabe2cd4adacaad5d7dbcaddaba4c14e37456
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-08 11:14:10 +0800

    質因數分解?

--- DS_pr/UVA294_N.py
+++ DS_pr/UVA294_N.py
@@ -1,11 +1,31 @@
 n=int(input())
+grid=[1,2]
+for i in range(3,30001,2):
+    check=1
+    for j in range(3,int(i**0.5)+1):
+        if i%j==0:
+            check=0
+            break
+    if check:
+        grid.append(i)
+print(grid)
+ans=[1,1]
 for i in range(n):
     s,e=map(int,input().split())
-    ans=0
     for j in range(s,e+1):
-        temp=0
-        for x in range(2,j):
-            if j%x==0:
-                temp+=1
-        if temp>ans:
-            ans=temp
+        if j in grid:
+            continue
+        elif j==1:
+            continue
+        else:
+            temp=j
+            # print(j)
+            count=0
+            now=0
+            while temp>=grid[now]:
+                if temp%grid[now]==0:
+                    count+=1
+                now+=1
+            if count>ans[1]:
+                ans=[j,count]
+    print(ans)
