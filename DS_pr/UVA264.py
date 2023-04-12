ca0e695088e51b6851ff27a4aabe2f13969ebc9f
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-12 08:35:01 +0800

    UVA264 12149 AC

--- DS_pr/UVA264.py
+++ DS_pr/UVA264.py
@@ -1,42 +1,22 @@
-grid=[[1,1,1,1]]
+#想規律不要硬炸
 while True:
     try:
         n=list(map(int,input().split()))
         for i in n:
-            if len(grid)>=i:
-                print("TERM %d IS %d/%d"%(i,grid[i-1][0],grid[i-1][1]))
+            temp=i
+            count,ckL=1,0
+            while True:
+                if temp-count<=0:
+                    ckL=count
+                    break
+                temp-=count
+                count+=1
+            #print(ckL)
+            #print(temp)
+            if ckL%2==0:
+                print("TERM %d IS %d/%d"%(i,1+temp-1,ckL-temp+1))
             else:
-                l,r,w=grid[len(grid)-1][0],grid[len(grid)-1][1],grid[len(grid)-1][2]
-                ck=grid[len(grid)-1][3]
-                for j in range(i-len(grid)):
-                    if l==1:
-                        if ck:
-                            r+=1
-                            ck=False
-                        else:
-                            r-=1
-                            l+=1
-                            w=0
-                            ck=True
-                    elif r==1:
-                        if ck:
-                            l+=1
-                            ck=False
-                        else:
-                            l-=1
-                            r+=1
-                            w=1
-                            ck=True
-                    else:
-                        if w:
-                            r+=1
-                            l-=1
-                        else:
-                            r-=1
-                            l+=1
-                    #print("%d %d"%(l,r))
-                    grid.append([l,r,w,ck])
-                print("TERM %d IS %d/%d"%(i,l,r))
+                print("TERM %d IS %d/%d"%(i,ckL-temp+1,1+temp-1))
 
     except EOFError:
         break
