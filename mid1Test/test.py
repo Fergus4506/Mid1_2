faf34b77817674ebaf37f0af69423dccbf893a89
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-17 17:47:20 +0800

    123

--- mid1Test/test.py
+++ mid1Test/test.py
@@ -155,17 +155,52 @@
 # 	except EOFError:
 # 		break
 
-def feyman(num):
-    if num==1:
-        return 1
-    else:
-        return num*num+feyman(num-1)
+# def feyman(num):
+#     if num==1:
+#         return 1
+#     else:
+#         return num*num+feyman(num-1)
+# while True:
+#     try:
+#         n=int(input())
+#         if n==0:
+#             break
+#         print(feyman(n))
+#     except:
+#         break
+# #!/bin/bash
+
+sentences=[]
 while True:
-    try:
-        n=int(input())
-        if n==0:
-            break
-        print(feyman(n))
-    except:
-        break
-#!/bin/bash
+	try:
+		s=input()
+		sentences.append(s)
+	except EOFError:
+		break
+
+s_long=len(sentences)
+longest=len(sentences[0])
+for i in range(len(sentences)):
+	if len(sentences[i])>longest:
+		longest=len(sentences[i])
+		
+temp=[[""]*s_long for i in range(longest)]
+for i in range(len(sentences)-1,-1,-1):
+	subsen=sentences[i]
+	for j in range(len(subsen)):
+		temp[j][i]=subsen[j]
+#print(temp)
+count=0#計算超過字串長度的部分
+for i in range(len(temp)):
+	for j in range(len(temp[i])-1,0,-1):
+		if temp[i][j]=="":#字串如果已超出範圍則會抓到""，就需要加入計算
+			count+=1
+		elif count!=0:
+			for z in range(count):
+				print(" ",end="")#如果找到還有字元的長度，確認前面有沒有空格(已經超過字串範圍的格子)
+                                 #在將它輸出
+			count=0
+			print(temp[i][j],end="")
+		else:
+			print(temp[i][j],end="")
+	print(temp[i][0])
