2fa1bbb2ff45f67ca200bcc7b527a63a2e32dc76
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-19 09:49:17 +0800

    UVA382 maybe AC,wait for UVA open

---
+++ DS_pr/UVA382.py
@@ -0,0 +1,29 @@
+count=0
+while True:
+    n=list(map(int,input().split()))
+    if not count:
+        count=1
+        print("PERFECTION OUTPUT")
+    check=0
+    for i in n:
+        if i==0:
+            check=1
+            break
+        cha=0
+        for j in range(1,i):
+            if i%j==0:
+                cha+=j
+                #print(j)
+            if cha>i:
+                print("{:5d}  ABUNDANT".format(i))
+                break
+        if cha==i:
+            print("{:5d}  PERFECT".format(i))
+        elif cha<i:
+            print("{:5d}  DEFICIENT".format(i))
+
+
+
+    if check:
+        print("END OF OUTPUT")
+        break
