1e9799fc73966b69555022051551aac6f2073414
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-04-24 16:19:46 +0800

    PB upload

---
+++ mid2Test/PB/PB.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+        st=list(map(int,input().split()))
+        goodP=list(map(int,input().split()))
+        goodP.sort()
+        gf=list(map(int,input().split()))
+        gf.sort()
+        i,j,ans=0,0,0
+        while i<len(goodP):
+            if goodP[i]>=gf[j]:
+                ans+=goodP[i]
+                j+=1
+            else:
+                i+=1
+        print(ans)
+            
+
+    except EOFError:
+        break
