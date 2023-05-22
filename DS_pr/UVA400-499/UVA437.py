2a219649d5aa5497c5390f058ed73ecb49fbcd20
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-05-23 07:26:26 +0800

    [upload]UVA439 AC

--- DS_pr/UVA400-499/UVA437.py
+++ DS_pr/UVA400-499/UVA437.py
@@ -1,5 +1,7 @@
 count=1
 #題目為動態規劃，顧名思義就是利用前面已做過的資料去判斷後續資料的變化為何
+#此題利用LIS來解題，LIS為最長遞增子字串的簡稱，利用前面算好的子字串長度去判斷接下來的方塊
+#要去拿哪一個已堆積好總高度才會符合最大高度
 while True:
     n=int(input())
     if n==0:
@@ -26,4 +28,3 @@ while True:
         #     print(LIS[i])
     print("Case %d: maximum height = %d"%(count,max(LIS)))
     count+=1
-
--- DS_pr/UVA400-499/UVA439.py
+++ DS_pr/UVA400-499/UVA439.py
@@ -1,46 +1,56 @@
-def main():
-    while True:
-        try:
-            st,ed=input().split()
-            BFS=[]
-            BFS.append(st)
-            if st==ed:
-                print("To get from %s to %s takes 0 knight moves."%(st,ed))
-            else:
-                temp=[]
-                count=1
-                while True:
-                    for i in range(len(BFS)):
-                        if  ord(BFS[i][0])-2>=ord('a') and int(BFS[i][1])-1>=1:
-                            s=str(chr(ord(BFS[i][0])-2))+str(int(BFS[i][1])-1)
-                            temp.append(s)
-                        if  ord(BFS[i][0])-1>=ord('a') and int(BFS[i][1])-2>=1:
-                            s=str(chr(ord(BFS[i][0])-1))+str(int(BFS[i][1])-2)
-                            temp.append(s)
-                        if  ord(BFS[i][0])+1<=ord('h') and int(BFS[i][1])-2>=1:
-                            s=str(chr(ord(BFS[i][0])+1))+str(int(BFS[i][1])-2)
-                            temp.append(s)
-                        if  ord(BFS[i][0])+2<=ord('h') and int(BFS[i][1])-1>=1:
-                            s=str(chr(ord(BFS[i][0])+2))+str(int(BFS[i][1])-1)
-                            temp.append(s)
-                        if  ord(BFS[i][0])+2<=ord('h') and int(BFS[i][1])+1<=8:
-                            s=str(chr(ord(BFS[i][0])+2))+str(int(BFS[i][1])+1)
-                            temp.append(s)
-                        if  ord(BFS[i][0])+1<=ord('h') and int(BFS[i][1])+2<=8:
-                            s=str(chr(ord(BFS[i][0])+1))+str(int(BFS[i][1])+2)
-                            temp.append(s)
-                        if  ord(BFS[i][0])-1>=ord('a') and int(BFS[i][1])+2<=8:
-                            s=str(chr(ord(BFS[i][0])-1))+str(int(BFS[i][1])+2)
-                            temp.append(s)
-                        if  ord(BFS[i][0])-2>=ord('a') and int(BFS[i][1])+1<=8:
-                            s=str(chr(ord(BFS[i][0])-2))+str(int(BFS[i][1])+1)
-                            temp.append(s)
-                    #print(*temp)
-                    if ed in temp:
+
+while True:
+    try:
+        st,ed=input().split()
+        f=ord(st[0])
+        s=int(st[1])
+        ef=ord(ed[0])
+        es=int(ed[1])
+        BFS=[]
+        BFS.append([f,s])
+        if st==ed:
+            print("To get from %s to %s takes 0 knight moves."%(st,ed))
+        else:
+            temp=[]
+            count=1
+            check=0
+            while True:
+                # print(BFS)
+                for i in range(len(BFS)):
+                    if  BFS[i][0]-2>=ord('a') and BFS[i][1]-1>=1:
+                        temp.append([BFS[i][0]-2,BFS[i][1]-1])
+
+                    if  BFS[i][0]-1>=ord('a') and BFS[i][1]-2>=1:
+                        temp.append([BFS[i][0]-1,BFS[i][1]-2])
+
+                    if  BFS[i][0]+1<=ord('h') and BFS[i][1]-2>=1:
+                        temp.append([BFS[i][0]+1,BFS[i][1]-2])
+
+                    if  BFS[i][0]+2<=ord('h') and BFS[i][1]-1>=1:
+                        temp.append([BFS[i][0]+2,BFS[i][1]-1])
+
+                    if  BFS[i][0]+2<=ord('h') and BFS[i][1]+1<=8:
+                        temp.append([BFS[i][0]+2,BFS[i][1]+1])
+
+                    if  BFS[i][0]+1<=ord('h') and BFS[i][1]+2<=8:
+                        temp.append([BFS[i][0]+1,BFS[i][1]+2])
+
+                    if  BFS[i][0]-1>=ord('a') and BFS[i][1]+2<=8:
+                        temp.append([BFS[i][0]-1,BFS[i][1]+2])
+
+                    if  BFS[i][0]-2>=ord('a') and BFS[i][1]+1<=8:
+                        temp.append([BFS[i][0]-2,BFS[i][1]+1])
+
+                for i in range(len(temp)):   
+                    if temp[i][0]==ef and temp[i][1]==es:
                         print("To get from %s to %s takes %d knight moves."%(st,ed,count))
+                        check=1
                         break
-                    count+=1
-                    BFS=temp
-        except EOFError:
-            break
-main()
+                if check:
+                    break
+                #print(*temp)
+                count+=1
+                BFS=temp
+                temp=[]
+    except EOFError:
+        break
