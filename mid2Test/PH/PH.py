7093ad2c9859d12be075d2e79fcd47f09b7e1c1e
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-06-12 19:21:16 +0800

    [PH] need 2b search

--- mid2Test/PH/PH.py
+++ mid2Test/PH/PH.py
@@ -40,17 +40,24 @@
 
 #     except EOFError:
 #         break
+
+#概念對了，但是在確定高度的地方一定要用二分搜尋法來確定才可以，因為搜尋元素一多會導致
+#該程式出現TIM的問題
 while True:
     try:
         n,p=map(int,input().split())
         grid=list(map(int,input().split()))
         post=list(map(int,input().split()))
+        for i in range(len(post)):
+            if post[i]==0:
+                post[i]=1
         maxN=max(grid)
         while True:
             #print(maxN)
             nowp=0
             pt=0
             check=0
+            #第一個for迴圈的目的在於確定是否以現在maxN的高度下是否可以把所有海報成功貼上去
             for i in range(len(grid)):
                 if grid[i]>=maxN:
                     nowp+=1
@@ -62,11 +69,13 @@ while True:
                 if pt==len(post):
                     check=1
                     break
+            #如果可以這裡就會輸出
             if check:
                 print(maxN)
                 break
             else:
                 temp=0
+                #循序搜尋下一個高度=>這裡改成二分搜就可以解決(搜尋的時間問題加上py的慢)
                 for i in range(len(grid)):
                     if grid[i]<maxN and grid[i]>temp:
                         temp=grid[i]
