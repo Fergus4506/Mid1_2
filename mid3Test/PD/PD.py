ded45c322012e7abd514f41fa7c497221e583c4e
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-06-14 09:04:12 +0800

    [upload] PD test1

---
+++ mid3Test/PD/PD.py
@@ -0,0 +1,16 @@
+def crht(grid):
+    
+# def incode():
+# def decode():
+while True:
+    try:
+        tree=input().split()
+        n=int(input())
+        for i in range(n):
+            case=input()
+            if case.isdigit():
+                # decode()
+            else:
+                # incode()
+    except EOFError:
+        break
