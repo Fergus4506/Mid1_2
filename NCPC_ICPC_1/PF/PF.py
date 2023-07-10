b487073ace68d16a29515b9adc3888cbfa821698
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-07-10 16:20:27 +0800

    [upload] PH python AC

--- NCPC_ICPC_1/PF/PF.py
+++ NCPC_ICPC_1/PF/PF.py
@@ -3,11 +3,13 @@ allAnimal=[]
 for i in range(n):
     allAnimal.append(int(input()))
 allAnimal.sort()
-print(*allAnimal)
-maxNumber=allAnimal[len(allAnimal)-1]
-sumGrid=[]
-temp=0
-for i in range(len(allAnimal)):
-    temp+=allAnimal[i]
-    sumGrid.append([temp,allAnimal[i]])
-print(sumGrid)
+minN,maxN=allAnimal[0],allAnimal[len(allAnimal)-1]
+leght,right=0,len(allAnimal)-1
+while minN!=maxN:
+    if maxN>minN:
+        leght+=1
+        minN+=allAnimal[leght]
+    elif minN>maxN:
+        right+=1
+        maxN+=allAnimal[right]
+print(allAnimal[leght]+1)
--- NCPC_ICPC_1/PH/PH.py
+++ NCPC_ICPC_1/PH/PH.py
@@ -1,13 +1,15 @@
+
 def main():    
+    count=1
     n,m=map(int,input().split())
     grid=[[]for i in range(n)]
     color=0
 
     #取值，因為是無像圖所以圖形上雙向皆要能通行
     for i in range(m):
-        t=list(map(int,input().split()))
-        grid[t[0]-1].append(t[1]-1)
-        grid[t[1]-1].append(t[0]-1)
+        t1,t2=map(int,input().split())
+        grid[t1-1].append(t2-1)
+        grid[t2-1].append(t1-1)
         color=(color+1)%2
 
     # print(grid)
@@ -18,28 +20,24 @@ def main():
     #並且要注意因為這題為無向圖所以不能讓他跑進迴圈中，當判斷到要準備跑到以跑過的節點時就要忽略該資料
     queue=[]
     queue.append(0)
-    np=[]#紀錄走過的節點
+    np=[0 for i in range(n)]#紀錄走過的節點
     ans=1
-    check=0
     while len(queue):
+        #print(np)
         zz=queue#該層資料
         for x in range(len(zz)):
             now=zz.pop(0)
-
             #去抓這層queue中的第一筆資料並去判斷牠的子節點是否符合條件
             for i in range(len(grid[now])):
-                if grid[now][i] in np:#子節點已走過則忽略
-                    continue
-                elif grid[now][i]==n-1:#該子節點為終點，輸出答案並跳出迴圈
-                    # print(now)
+                if grid[now][i]==n-1:#該子節點為終點，輸出答案並跳出迴圈
+                    #print(now)
                     print(ans-1)
-                    check=1
-                    break
-                np.append(grid[now][i])
+                    return None
+                elif np[grid[now][i]]:#子節點已走過則忽略(不能將所有走過的點用陣列儲存後再用 in 檢索，因為這樣會超時)
+                    continue
+                np[grid[now][i]]=1
                 queue.append(grid[now][i])
-            if check:
-                break
+        # print(queue)
         ans+=1
-        if check:
-            break
 main()
+
