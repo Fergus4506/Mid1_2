72b7323f3554452d5cdfb388e46ecb5cf9c2418e
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-11-17 14:03:57 +0800

    [upload]PB AC

--- AG/Algorithms_Mid_Exam _2/PB/PB.py
+++ AG/Algorithms_Mid_Exam _2/PB/PB.py
@@ -3,26 +3,33 @@ count=0
 def checkDFS(grid,nowSim,ans,last):
     #print(nowSim)
     if(len(grid)==0):
-        if nowSim not in ans and (last+1) in prime:
+        if (last+1) in prime:
             ans.append(nowSim)
     else:
         for i in range(len(grid)):
             if (last+grid[i]) in prime:
-                tempSim=nowSim
-                tempSim+=str(grid[i])
+                tempSim=nowSim[:]
+                tempSim.append(grid[i])
                 tgrid=grid[:]
                 tgrid.pop(i)
                 checkDFS(tgrid,tempSim,ans,grid[i])
 while 1:
     try:
         count+=1
-        print("Case %d:"%(count))
         N=int(input())
+        if(count>1):
+            print()
+        print("Case %d:"%(count))
         grid=[i+1 for i in range(N)]
         grid.pop(0)
         ans=[]
-        checkDFS(grid,"1",ans,1)
+        checkDFS(grid,[1],ans,1)
+        # print(ans)
         for i in ans:
-            print(" ".join(i))
+            for j in range(len(i)):
+                if j==len(i)-1:
+                    print(i[j])
+                else:
+                    print(i[j],end=" ")
     except EOFError:
         break
