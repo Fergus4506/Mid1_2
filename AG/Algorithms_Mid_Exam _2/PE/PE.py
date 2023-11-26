49ce643a4ca70273bfccefc18ba526ee84891a31
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-11-26 12:22:36 +0800

    [upload] PE AC

---
+++ AG/Algorithms_Mid_Exam _2/PE/PE.py
@@ -0,0 +1,17 @@
+while 1:
+    try:
+        temp=input()
+        n=list(map(int,temp[1:-1].split(',')))
+        # print(n)
+        ans=[]
+        ans.append(n[0])
+        ans.append(n[1])
+        ans.append(n[2])
+        for i in range(3,len(n)):
+            tempSum=[]
+            for j in range(i-3+1):
+                tempSum.append(ans[j]+n[i])
+            ans.append(max(tempSum))
+        print(max(ans))
+    except EOFError:
+        break
