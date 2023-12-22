4a828091b4f46c73c4c999daf5218449c532299a
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-12-22 09:30:40 +0800

    [RSA upload] mebay AC??

--- RSA_test.py
+++ RSA_test.py
@@ -1,13 +1,79 @@
-import random
-e=int(input())
-p,q=map(int,input().split())
-M=input()
-n=p*q
-# e=
-MChangeGrid=""
-for i in M:
-    if i==" ":
-        MChangeGrid+="26"
-    else:
-        MChangeGrid+=str(ord(i)-ord('A')).zfill(2)
-print(MChangeGrid)
+#這部分的函式為輾轉相除法找反元素的部分
+#利用輾轉相除中gcd(a,b)=sa+tb的部分來解決找尋互質的數字
+#其中的S也就是下面變數的X1=>也就是算到最後對於a的反元素
+def mod_inverse(a, b):
+    m0, x0, x1 = b, 0, 1
+    while a > 1:
+        q = a // b
+        b, a = a % b, b
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
+
+
+def find_n_and_d(p, q, e):
+    n = p * q
+    f_n = (p - 1) * (q - 1)
+
+    d = mod_inverse(e, f_n)
+
+    return n, d
+
+def encrypt(ms, e, n):
+    chrGrid = []
+    t1=0
+    t2=0
+    ct=0
+    if len(ms)%2!=0:
+        ms+=" "
+    for char in ms:
+        if char == " ":
+            char_value = 26
+        else:
+            char_value = ord(char) - ord('A')
+        ct+=1
+        if ct==2:
+            t2=char_value
+            chrGrid.append(t1*100+t2)
+            ct=0
+        else:
+            t1=char_value
+    print(chrGrid)
+    enGrid = [(pow(char, e, n)) for char in chrGrid]
+    print(enGrid)
+    return enGrid
+
+def decrypt(enGrid, d, n):
+    deGrid = [(pow(char, d, n)) for char in enGrid]
+    print(deGrid)
+    de_ms = ""
+    for i in range(0, len(deGrid)):
+        if deGrid[i]//100== 26:
+            de_ms += " "
+        else:
+            de_ms += chr(deGrid[i]//100 + ord('A'))
+        if deGrid[i]%100== 26:
+            de_ms += " "
+        else:
+            de_ms += chr(deGrid[i] %100 + ord('A'))
+        
+    
+    return de_ms
+
+def main():
+    e = int(input(""))
+    p = int(input(""))
+    q = int(input(""))
+
+    n, d = find_n_and_d(p, q, e)
+    print(f"(n, e): ({n}, {e})")
+    print(f"(n, d): ({n}, {d})")
+
+    ms = input()
+    en_ms = encrypt(ms.upper(), e, n)
+    print("encryption:", en_ms)
+
+    de_ms = decrypt(en_ms, d, n)
+    print("decryption:", de_ms)
+
+if __name__ == "__main__":
+    main()
